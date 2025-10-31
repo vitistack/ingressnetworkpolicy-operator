c1a76aa51013246d74db0fe4907ec402887dd508
Frode Eriksen <frode@eriksen.casa>
2025-10-31 10:38:08 +0100

    Make build-installer

--- internal/controller/networkpolicy_controller.go
+++ internal/controller/networkpolicy_controller.go
@@ -36,9 +36,9 @@ type NetworkPolicyReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=ingressnetworkpolicies.vitistack.io,resources=networkpolicies,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=ingressnetworkpolicies.vitistack.io,resources=networkpolicies/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=ingressnetworkpolicies.vitistack.io,resources=networkpolicies/finalizers,verbs=update
+// +kubebuilder:rbac:groups="",resources=networkpolicies,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=networkpolicies/status,verbs=get;list;watch
+// +kubebuilder:rbac:groups="",resources=networkpolicies/finalizers,verbs=list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
