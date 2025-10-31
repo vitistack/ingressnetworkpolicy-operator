95e70346442b6173390b3debb561d9e1bc755285
Frode Eriksen <frode@eriksen.casa>
2025-10-31 16:04:55 +0100

    Moved NetworkPolicies to NS networkpolicies

--- internal/controller/contants.go
+++ internal/controller/contants.go
@@ -1,7 +1,7 @@
 package controller
 
 const (
-	DefaultNamespace         = "default"
+	DefaultNamespace         = "networkpolicies"
 	AnnotationNginxWhitelist = "nginx.ingress.kubernetes.io/whitelist-source-range"
 	AnnotationNetworkPolicy  = "networking.k8s.io/policy"
 	AnnotationWhitelist      = "networking.k8s.io/whitelist"
